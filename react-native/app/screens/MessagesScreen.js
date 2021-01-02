c575f7515e27ad3010414100d0ff9cbc48aa5b9e
Amine <aminemouhaouir1996@gmail.com>
2021-01-02 16:30:16 +0100

    1st commit

---
+++ react-native/app/screens/MessagesScreen.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+
+import Screen from "../components/Screen";
+import {
+  ListItem,
+  ListItemDeleteAction,
+  ListItemSeparator,
+} from "../components/lists";
+
+const initialMessages = [
+  {
+    id: 1,
+    title: "User1",
+    description: "Hey! Is this item still available?",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 2,
+    title: "User2",
+    description:
+      "I'm interested in this item. When will you be able to post it? please call me 0623124578",
+    image: require("../assets/mosh.jpg"),
+  },
+];
+
+function MessagesScreen(props) {
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDelete = (message) => {
+    // Delete the message from messages
+    setMessages(messages.filter((m) => m.id !== message.id));
+  };
+
+  return (
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+            onPress={() => alert(item.description,)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              title: "T2",
+              description: "D2",
+              image: require("../assets/mosh.jpg"),
+            },
+          ]);
+        }}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default MessagesScreen;
